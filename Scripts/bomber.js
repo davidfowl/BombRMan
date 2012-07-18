290e21d322cc68fcb3361937902168e67d71a0d4
David Fowler <davidfowl@gmail.com>
2012-07-18 16:39:37 -0700

    Make some changes to movement, not 100% yet. Still some hairy situations where you can get stuck.

--- Scripts/bomber.js
+++ Scripts/bomber.js
@@ -201,7 +201,10 @@
                     top: sourceTop,
                     right: sourceLeft + game.map.tileSize,
                     bottom : sourceTop + game.map.tileSize
-                };
+                },
+                collisions = [],
+                possible = [],
+                handled = false;
 
             window.Game.Logger.clear();
             window.Game.Logger.log('source=' + JSON.stringify(sourceRect));
@@ -209,10 +212,11 @@
             window.Game.Logger.log('actualX=' + actualX + ', actualY=' + actualY);
             window.Game.Logger.log('directionX=' + this.directionX + ', directionY=' + this.directionY);
 
-            var collisions = [];
             for(var i = 0; i < targets.length; ++i) { 
                 var tx = targets[i].x,
                     ty = targets[i].y,
+                    targetX = actualX + tx,
+                    targetY = actualY + ty,
                     left = (actualX + tx) * game.map.tileSize,
                     top = (actualY + ty) * game.map.tileSize,
                     targetRect = {
@@ -226,9 +230,12 @@
                     intersects = window.Game.Utils.intersects(sourceRect, targetRect);
 
                 if(!movable && intersects) {
-                    collisions.push({ x: actualX + tx, y: actualY + ty });
+                    collisions.push({ x: targetX, y: targetY });
 
                     window.Game.Logger.log('collision=(' + (actualX + tx) + ', ' + (actualY + ty) +')');
+                }
+                else {
+                    possible.push({ x: targetX, y: targetY });
                 }
             }
 
@@ -248,14 +255,42 @@
 
                     if(absY >= 35 && absY < 100) {
                         this.exactY += DELTA * -window.Game.Utils.sign(diffY);
+                        handled = true;
                     }
 
                     if(absX >= 35  && absX < 100) {
                         this.exactX += DELTA * -window.Game.Utils.sign(diffX);
+                        handled = true;
                     }
 
                     window.Game.Logger.log('diffX=(' + absX +', diffY=' + absY + ')');
                 break;
+            }
+
+            if(!handled && collisions.length > 0) {
+                var candidate,
+                    p1 = { x: actualX + this.directionX, y: actualY },
+                    p2 = { x: actualX, y: actualY + this.directionY };
+
+                for(var i = 0; i < possible.length; ++i) { 
+                    if(possible[i].x === p1.x && possible[i].y === p1.y) {
+                        // window.Game.Logger.log('p['+ i + ']=(' + possible[i].x +', ' + possible[i].y + ')');
+                        candidate = { x: this.directionX, y: 0 };
+                    }
+                    
+                    if(possible[i].x === p2.x && possible[i].y === p2.y) {
+                        candidate = { x: 0, y: this.directionY };
+                    }
+
+                    if(candidate) {
+                        this.exactX += DELTA * candidate.x;
+                        this.exactY += DELTA * candidate.y;
+
+                        this.x = actualX;
+                        this.y = actualY;
+                        break;
+                    }
+                }
             }
         },
         moveTo: function (x, y) {
